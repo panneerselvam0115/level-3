a5c5c96917384cf267c2a16c2bb232387773764e
panneerselvam0115 <smilyboyselvam61@gmail.com>
2025-10-31 11:03:55 +0530

    program 11

---
+++ program 11.py
@@ -0,0 +1,11 @@
+print("TAKSHASHILA UNIVERSITY")
+print(".......................")
+print(".......................")
+print("student mark list")
+m1=int(input("enter the python mark:"))
+m2=int(input("enter the dbms mark:"))
+m3=int(input("enter the accounts mark:"))
+total=m1+m2+m3
+print("total:",total)
+average=total/3
+print("average mark:",average)
